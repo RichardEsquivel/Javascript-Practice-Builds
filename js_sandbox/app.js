1bdd6018d686e2e68568f0b5591b601ec31cb97a
RichardEsquivel <rlesquivel@gmail.com>
2019-11-26 17:12:12 -0600

    Building out basic folder structure for practice

---
+++ js_sandbox/app.js
@@ -0,0 +1,37 @@
+//single line comments
+
+//This is a lot of utilizations of console.log
+// console.log("WHAT A COOL LOG!", 'Hello World');
+// console.log(123);
+// console.log(true);
+
+// var greeting = 'Hello';
+// console.log(greeting);
+// console.log([1, 2, 3, 4]);
+// console.log({ a: 1, b: 2 });
+// console.table({ 1: 1, b: 2 });
+
+
+// console.error('This is a cool error!');
+// console.clear();
+// console.warn('This is a BIG WARNING!');
+// console.time('Hello');
+// console.log("WHAT A COOL LOG!", 'Hello World');
+// console.log("WHAT A COOL LOG!", 'Hello World');
+// console.log("WHAT A COOL LOG!", 'Hello World');
+// console.log("WHAT A COOL LOG!", 'Hello World');
+// console.log("WHAT A COOL LOG!", 'Hello World');
+// console.log("WHAT A COOL LOG!", 'Hello World');
+// console.log("WHAT A COOL LOG!", 'Hello World');
+// console.log("WHAT A COOL LOG!", 'Hello World');
+// console.timeEnd('Hello');
+/*
+multi line
+comments
+*/
+
+
+//var, let, const : 3 keywords var has been around since the beginning, let and const es2015 or es6 update to language going to stay away from var, let and const have big advantage regarding block level scope
+
+var name = 'John Doe';
+console.log(name);
